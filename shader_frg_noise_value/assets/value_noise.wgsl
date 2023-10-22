0bb8dbbc9f52a1b9d5691cc34a99f7f5fd6b4223
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2023-10-22 20:21:30 +0200

    Add octaves to noise value

--- shader_frg_noise_value/assets/value_noise.wgsl
+++ shader_frg_noise_value/assets/value_noise.wgsl
@@ -1,12 +1,14 @@
 #import bevy_sprite::mesh2d_vertex_output  MeshVertexOutput
 #import bevy_sprite::mesh2d_view_bindings globals
 
-struct CustomMaterial {
-    frequence: f32,
-};
-
 @group(1) @binding(0)
-var<uniform> material: CustomMaterial;
+var<uniform> octaves: u32;
+@group(1) @binding(1)
+var<uniform> base_frequence: f32;
+@group(1) @binding(2)
+var<uniform> lacunarity: f32;
+@group(1) @binding(3)
+var<uniform> gain: f32;
 
 fn random (uv: vec2<f32>) -> f32 {
     return fract(sin(dot(uv.xy, vec2(12.9898,78.233))) * 43758.5453123);
@@ -37,7 +39,19 @@ fn average_noise_smoothstep(scaled_uv: vec2<f32>) -> f32 {
 fn fragment(in: MeshVertexOutput) -> @location(0) vec4<f32> {
     let size = in.position.xy/in.uv;
     let ratio = size.x/size.y;
-    let scaled_u = in.uv.x * material.frequence * ratio;
-    let scaled_v = in.uv.y * material.frequence;
-    return vec4<f32>(vec3(average_noise_smoothstep(vec2(scaled_u, scaled_v))), 1.0);
+    var scaled_u = in.uv.x * base_frequence * ratio;
+    var scaled_v = in.uv.y * base_frequence;
+    var noise_val = 0.0;
+    var freq = base_frequence;
+    var amp = 1.0;
+    var total_amplitude = 0.0;
+    for (var i = u32(0); i < octaves; i++) {
+        total_amplitude += amp;
+        noise_val += amp * average_noise_smoothstep(vec2(scaled_u, scaled_v));
+        amp *= gain;
+        scaled_u *= lacunarity;
+        scaled_v *= lacunarity;
+    }
+    noise_val /= total_amplitude;
+    return vec4<f32>(vec3(noise_val), 1.0);
 }
--- shader_frg_noise_value/src/main.rs
+++ shader_frg_noise_value/src/main.rs
@@ -46,7 +46,23 @@ fn system_gui(mut contexts: EguiContexts, mut materials: ResMut<Assets<CustomMat
     egui::Window::new("Noise configuration")
         .resizable(false)
         .show(ctx, |ui| {
-            ui.add(egui::DragValue::new(&mut material.frequence));
+            egui::Grid::new("some_unique_id").show(ui, |ui| {
+                ui.label("Octaves");
+                ui.add(egui::DragValue::new(&mut material.octaves));
+                ui.end_row();
+                ui.label("Initial frequence");
+                ui.add(
+                    egui::Slider::new(&mut material.base_frequence, 0.001..=1000.0)
+                        .logarithmic(true),
+                );
+                ui.end_row();
+                ui.label("Frequence increase factor");
+                ui.add(egui::Slider::new(&mut material.lacunarity, 1.0..=1000.0).logarithmic(true));
+                ui.end_row();
+                ui.label("Amplitude decrease factor");
+                ui.add(egui::Slider::new(&mut material.gain, 0.001..=1.0).logarithmic(true));
+                ui.end_row();
+            });
         });
 }
 
@@ -54,12 +70,23 @@ fn system_gui(mut contexts: EguiContexts, mut materials: ResMut<Assets<CustomMat
 #[uuid = "3bb72d82-d404-42e1-b225-2b1debd79518"]
 struct CustomMaterial {
     #[uniform(0)]
-    frequence: f32,
+    octaves: u32,
+    #[uniform(1)]
+    base_frequence: f32,
+    #[uniform(2)]
+    lacunarity: f32,
+    #[uniform(3)]
+    gain: f32,
 }
 
 impl Default for CustomMaterial {
     fn default() -> Self {
-        Self { frequence: 10.0 }
+        Self {
+            octaves: 1,
+            base_frequence: 1.0,
+            lacunarity: 2.0,
+            gain: 0.5,
+        }
     }
 }
 
